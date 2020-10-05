bf6835494c0f038ce58ff6351f2beac44abab645
Chripaz <chripaz@gmail.com>
2020-10-05 02:48:20 +0100

    adding new files

---
+++ hello.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  profile = "default"
+
+  region = "eu-west-1"
+
+  }
+
+  terraform {
+         backend "s3" {
+        bucket = "chripaz-simple-bucket"
+        key = "chris.tfstate"
+        region = "eu-west-1"
+    }    
+  }
+resource "aws_instance" "First_instance" {
+  ami                    = "ami-06072fd101b82e654"
+  instance_type          = "t2.micro"
+  key_name               = "devopsnii"
+  vpc_security_group_ids = ["sg-0e73bec63944bc1e8"]
+
+  tags = {
+    name        = "Our first node"
+    provisioner = "Terraform"
+
+
+  }
+
+}
